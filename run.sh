e670e2b7666c031ded5fdc5ccaf77dc532852310
Jason Fleisher <github@jasonfleisher.com>
2020-04-30 08:29:25 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=latest
+version=3.2.10
 
 mkdir -p /opt/tplink/$version/OmadaController/data
 mkdir -p /opt/tplink/$version/OmadaController/work
@@ -23,7 +23,7 @@ docker run -d \
 --name omada-controller \
 --network host \
 --memory 900m \
--e TZ=Europe/Amsterdam \
+-e TZ=America/Phoenix \
 -p 8088:8088   -p 8043:8043   -p 27001:27001/udp   -p 27002:27002   -p 29810:29810/udp   -p 29811:29811   -p 29812:29812   -p 29813:29813  \
 -v $omadadata:/opt/tplink/OmadaController/data   -v $omadawork:/opt/tplink/OmadaController/work   -v $omadalogs:/opt/tplink/OmadaController/logs omada-controller:$version
 # --network and --memory are optional
